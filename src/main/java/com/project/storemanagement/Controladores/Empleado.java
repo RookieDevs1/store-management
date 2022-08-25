b9e67e055ed775385c78d31c048d61ff46b6e3f1
jesenia mendoza <luxemburgo10@gmail.com>
2022-08-25 01:02:36 -0500

    se crearon atributos de la clase empleado con sus respectivos constructores y los metodos setter y getter.

--- src/main/java/com/project/storemanagement/Controladores/Empleado.java
+++ src/main/java/com/project/storemanagement/Controladores/Empleado.java
@@ -1,4 +1,40 @@
 package com.project.storemanagement.Controladores;
 
 public class Empleado {
+    private String nombre;
+    private String email;
+    private String rol;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String email, String rol) {
+        this.nombre = nombre;
+        this.email = email;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
