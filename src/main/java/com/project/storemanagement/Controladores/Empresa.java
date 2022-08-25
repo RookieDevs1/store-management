783117bec93b9b535075044d956bccda5dbfc363
Isaura Eckar <isauraeckar@hotmai.com>
2022-08-25 00:43:37 -0500

    Se crearon los atributos de la clase Empresa, su constructor junto a sus Getters y Setters.

--- src/main/java/com/project/storemanagement/Controladores/Empresa.java
+++ src/main/java/com/project/storemanagement/Controladores/Empresa.java
@@ -1,5 +1,50 @@
 package com.project.storemanagement.Controladores;
 
 public class Empresa {
-    private String nombre,direccion,telefono,nit;
+    private String nombre;
+    private String direccion;
+    private int telefono;
+    private long nit;
+
+    public Empresa() {
+    }
+
+    public Empresa(String nombre, String direccion, int telefono, long nit) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.nit = nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public long getNit() {
+        return nit;
+    }
+
+    public void setNit(long nit) {
+        this.nit = nit;
+    }
 }
