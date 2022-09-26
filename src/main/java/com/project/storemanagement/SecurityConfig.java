b363575f1e5f4d018ffeb4dfe3e1c6160f44714b
IvanB3 <ivanbenavides098@gmail.com>
2022-09-26 15:49:49 -0500

    Agregado a SecurityConfig --> authorizeHttpRequests

--- src/main/java/com/project/storemanagement/SecurityConfig.java
+++ src/main/java/com/project/storemanagement/SecurityConfig.java
@@ -1,16 +1,25 @@
 package com.project.storemanagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Optional;
 
 @EnableWebSecurity
 public class SecurityConfig {
 
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            return http.oauth2Login()
-                    .and().build();
-        }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated());
+
+        return http.oauth2Login()
+                .and().build();
+    }
+
 }
