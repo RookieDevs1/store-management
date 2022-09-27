5027171e540bb817da75ad058183f3cd18fb951e
Isaura Eckar <isauraeckar@hotmai.com>
2022-09-27 00:07:14 -0500

    modifacion archivo procfile

--- src/main/java/com/project/storemanagement/SecurityConfig.java
+++ src/main/java/com/project/storemanagement/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.project.storemanagement;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static javax.xml.transform.OutputKeys.VERSION;
 
 @EnableWebSecurity
 public class SecurityConfig {
@@ -14,6 +17,13 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) -> authorize
                         .anyRequest().authenticated());
 */
+
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/vendor/**", "/imag/**", "/css/**", "/js/**", "/favicon.ico").permitAll();
+
+
+
         return http.oauth2Login()
                 .and().build();
     }
